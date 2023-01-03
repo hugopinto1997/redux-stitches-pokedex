b5409746fcacee02218041940fc777b492f7b75e
hugopinto1997 <hugopillo@hotmail.com>
2023-01-03 16:18:36 -0600

    feat(components/ui): :sparkles: add Input ui components

---
+++ src/components/ui/input/TextInput.ts
@@ -0,0 +1,9 @@
+import { styled } from 'app/stitches.config';
+
+export const Input = styled('input', {
+  fontFamily: 'inherit',
+  fontSize: '800',
+  padding: '$md $rg',
+  outline: '0',
+  border: '0',
+});
