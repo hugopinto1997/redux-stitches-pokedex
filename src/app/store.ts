0d2f523770d0d754178b3e47dc53e3d843b78b20
hugopinto1997 <hugopillo@hotmail.com>
2023-01-03 12:31:12 -0600

    feat: :sparkles: add redux store global setup

---
+++ src/app/store.ts
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-cycle */
+import { configureStore } from '@reduxjs/toolkit';
+
+import pokemonsReducer from '../features/pokemons/slice/pokemons-slice';
+
+const store = configureStore({
+  reducer: {
+    pokemons: pokemonsReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
