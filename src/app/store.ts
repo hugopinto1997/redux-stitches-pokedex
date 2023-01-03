65e4fdf3d863f51a23dd075788887f857c2ab3c0
hugopinto1997 <hugopillo@hotmail.com>
2023-01-03 15:42:42 -0600

    fix: :recycle: update store file imports

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-cycle */
 import { configureStore } from '@reduxjs/toolkit';
 
-import pokemonsReducer from '../features/pokemons/slice/pokemons-slice';
+import pokemonsReducer from '../features/pokemons/slice/pokemonsSlice';
 
 const store = configureStore({
   reducer: {
