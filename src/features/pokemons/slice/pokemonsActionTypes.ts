5aa01c7f89c0539d89e6e68eaa60db87065ae299
hugopinto1997 <hugopillo@hotmail.com>
2023-01-03 16:32:00 -0600

    feat(features/pokemons): :sparkles: add pokemons reducers action types

---
+++ src/features/pokemons/slice/pokemonsActionTypes.ts
@@ -0,0 +1,7 @@
+export const pokemonActions = {
+  GET_ALL_POKEMONS_THUNK: 'pokemons/get-all',
+  GET_POKEMON_BY_NAME_THUNK: 'pokemons/get-by-name',
+  GET_POKEMON_DETAIL_THUNK: 'pokemons/get-pokemon-detail',
+};
+
+export default pokemonActions;
