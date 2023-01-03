549ad243d81cc66eeae814f295dba2284b8e414c
hugopinto1997 <hugopillo@hotmail.com>
2023-01-03 16:29:34 -0600

    feat(features/pokemons): :label: add pokemons type definitions

---
+++ src/features/pokemons/types/pokemon.ts
@@ -0,0 +1,54 @@
+import { NamedAPIResource } from './service';
+
+export type PokemonAbility = {
+  is_hidden: boolean;
+  slot: number;
+  ability: NamedAPIResource;
+};
+
+export type PokemonMove = {
+  move: NamedAPIResource;
+};
+
+export type PokemonSprite = {
+  front_default: string;
+  front_shiny: string;
+  front_female: string;
+  front_shiny_female: string;
+  back_default: string;
+  back_shiny: string;
+  back_female: string;
+  back_shiny_female: string;
+  other: {
+    'official-artwork': Pick<PokemonSprite, 'front_default'>;
+    dream_world: Pick<PokemonSprite, 'front_default'>;
+  };
+};
+
+export type PokemonStat = {
+  base_stat: number;
+  effort: number;
+  stat: NamedAPIResource;
+};
+
+export type PokemonType = {
+  slot: number;
+  type: NamedAPIResource;
+};
+
+export type Pokemon = {
+  id: number;
+  name: string;
+  base_experience: number;
+  height: number;
+  is_default: boolean;
+  order: number;
+  weight: number;
+  abilities: PokemonAbility[];
+  forms: NamedAPIResource[];
+  moves: PokemonMove[];
+  sprites: PokemonSprite;
+  species: NamedAPIResource[];
+  stats: PokemonStat[];
+  types: PokemonType[];
+};
